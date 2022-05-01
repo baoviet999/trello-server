8267bf6d1555e01136ad20762967c9d0a3867d96
BaoViet <baobaovietviet123@gmail.com>
2022-05-01 15:10:43 +0700

    config babel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { a } from "./routers/v1/rou.js";
 
 const app = express();
 
@@ -6,7 +7,7 @@ const hostname = "localhost";
 const PORT = process.env.PORT || 9090;
 
 app.get("/", (req, res) => {
-    res.send("<h1>hello</h1>");
+    res.send("<h1>Hoc node js</h1>");
 });
-
+a()
 app.listen(PORT, hostname, () => console.log(`Connect at ${hostname} : ${PORT}`));
---
+++ src/routers/v1/index.js
@@ -0,0 +1,3 @@
+export const hadle = () => {
+    console.log('aaaa')
+}
